2c2b9792b5c694943578c1b7482298edef84592f
SurjeshYadavMasai <surjeshom@gmail.com>
2023-01-14 11:37:38 +0530

    Screen1Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
- import './App.css';
-import { Navbar } from './navbar/Navbar';
+
+import './App.css';
+import { Navbar } from './Navbar/Navbar';
+import AllRoutes from './Routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <AllRoutes />
     </div>
   );
 }
